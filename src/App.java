cb948d2b75ebffbfec443c8bbe8aa36442b5e7c8
Angelsalcedoo <angelsalcedo200712@gmail.com>
2025-02-28 13:33:34 -0500

    se crea la arquitectura java

--- src/App.java
+++ src/App.java
@@ -5,15 +5,15 @@ public class App {
     public static void main(String[] args) throws Exception {
        
      Scanner scanner = new Scanner(System.in);
-System.out.println("ingrese su nombre ");
-String Nombre = scanner.nextLine();
-System.out.println("ingrese su edad");
-int Edad = scanner.nextInt();
-System.out.println("ingrese su estatura");
- Nombre = scanner.nextLine();
- double Estatura = scanner.nextDouble();
+    System.out.println("ingrese su nombre ");
+    String Nombre = scanner.nextLine();
+    System.out.println("ingrese su edad");
+    int Edad = scanner.nextInt();
+    System.out.println("ingrese su estatura");
+    Nombre = scanner.nextLine();
+    double Estatura = scanner.nextDouble();
 
- System.out.println("Hola, " + Nombre + ". Tienes " + Edad + " años y mides " + Estatura + " metros.");
+    System.out.println("Hola, " + Nombre + ". Tienes " + Edad + " años y mides " + Estatura + " metros.");
     }
 
 }
